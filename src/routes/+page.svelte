0a05c61aab65ff7d4654dd423b20e5fdfb608ee7
Kento Ueda <beatweapon7th@gmail.com>
2023-10-18 03:16:33 +0900

    認証処理をサンプル通りに実装

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Welcome to SvelteAuth</h1>
+
+<button
+	class="border rounded p-2 mt-10 mb-10 bg-gray-800 text-white hover:bg-gray-700"
+	on:click={() => (window.location.href = '/auth')}
+>
+	Logout
+</button>
+
+<pre>
+  {JSON.stringify(data, null, 2)}
+</pre>
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { getFirebaseClient } from '$lib/firebase_client';
+	import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+	let form: HTMLFormElement;
+	async function login(): Promise<void> {
+		try {
+			const auth = getFirebaseClient();
+			if (auth.error) {
+				return alert('Error: ' + auth.msg);
+			}
+			const cred = await signInWithPopup(auth.data, new GoogleAuthProvider());
+			const token = await cred.user.getIdToken();
+			await auth.data.signOut();
+			const input = document.createElement('input');
+			input.type = 'hidden';
+			input.name = 'token';
+			input.value = token;
+			form.appendChild(input);
+			form.submit();
+		} catch (err) {
+			console.error(err);
+		}
+	}
+</script>
+
+<form method="post" bind:this={form} />
+<button on:click={login} class="border rounded p-2 mt-10 bg-gray-800 text-white hover:bg-gray-700">
+	Login using Google
+</button>
